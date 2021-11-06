3ba2104c5b6b327d304cf974b0e233d6a7997880
N-Hagerdorn <62390366+N-Hagerdorn@users.noreply.github.com>
2021-11-06 12:54:35 -0400

    Start and login activities

--- app/src/main/java/com/example/gosh_aep_vision/MainActivity.java
+++ app/src/main/java/com/example/gosh_aep_vision/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.gosh_aep_vision;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.gosh_aep_vision.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +17,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void m_loginBtn_onClick(View v) {
+        Intent loginIntent = new Intent(this, LoginActivity.class);
+        startActivity(loginIntent);
     }
 }
