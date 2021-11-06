eb9fab5f083edfaac12bb53c85f828dde75f3d1a
N-Hagerdorn <62390366+N-Hagerdorn@users.noreply.github.com>
2021-11-06 12:23:18 -0400

    Base application - build on this

--- app/src/main/java/com/example/gosh_aep_vision/MainActivity.java
+++ app/src/main/java/com/example/gosh_aep_vision/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.gosh_aep_vision;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
